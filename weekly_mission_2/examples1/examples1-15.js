16712efae3ffd522e45152b907a6f1fc40d61619
ghector6 <ghectormus@gmail.com>
2022-04-14 01:03:50 -0500

    Agregando example1-15 metodo sort(ordena en alfabeto)

---
+++ weekly_mission_2/examples1/examples1-15.js
@@ -0,0 +1,4 @@
+//Ejemplo 15: Uso de sort para ordenar elementos
+const products = ['Milk', 'Coffee', 'Sugar', 'Honey', 'Apple', 'warrot']
+console.log("Ejemplo 15: Elementos ordernados usando SORT")
+console.log(products.sort())
