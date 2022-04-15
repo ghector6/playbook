c2caa9ab7f04b035ed7d10bc73c2e4be0f3203f3
ghector6 <ghectormus@gmail.com>
2022-04-14 20:15:31 -0500

    Clases: Creando un objeto a partir de una clase

---
+++ weekly_mission_2/examples2/examples2-2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto a partir de una clase
+class Pet {
+}
+const myPet1 = new Pet() 
+const myPet2 = new Pet() // Puedo crear muchos objetos de la clase Pet
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet2) // un objeto de la clase Pet. Si declara un console log de unaclase que no está definida 
+//da not defined. 
