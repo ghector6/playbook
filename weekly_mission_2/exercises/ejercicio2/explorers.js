4e4afb267e2481e7ce4ad6ff27a7be10306babb5
ghector6 <ghectormus@gmail.com>
2022-04-18 19:33:00 -0500

    forEach: para nombre de los explorers

---
+++ weekly_mission_2/exercises/ejercicio2/explorers.js
@@ -0,0 +1,61 @@
+const explorers = [
+    {
+      name: " Satriani",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Vai",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Petrucci",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+   explorers.forEach(name => console.log(name.name)) //Imprimiendo nombre de explorers con forEach
