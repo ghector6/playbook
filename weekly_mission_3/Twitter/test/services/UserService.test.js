3b62559168188bf97325f3f8d4a7c823e10ffc41
ghector6 <ghectormus@gmail.com>
2022-04-20 16:59:29 -0500

    Agregando pruebas después de haber usado .not y .tobedefined

--- weekly_mission_3/Twitter/test/services/UserService.test.js
+++ weekly_mission_3/Twitter/test/services/UserService.test.js
@@ -1,1 +1,13 @@
 const UserService = require('./../../app/services/UserService')
+
+describe("Test for User Service ", () => {
+
+    test("Creating a a new user using the UserService method", () => {
+        const user = UserService.create(1, "ghector6", "Hector",)
+        expect(user.username).toBe("ghector6")
+        expect(user.name).toBe("Hector")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+
+    })
+})
