504e13efa10f7b57c7690881dfaa71e0817f631f
ghector6 <ghectormus@gmail.com>
2022-04-19 21:46:40 -0500

    Agregando clase, metodo y sus pruebas funcionando

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('./../app/spiderman')
+
 describe("Unit Tests for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
         const andrewGarfield = new Spiderman("Amazing Spiderman", 31, "Andrew Garfield", 2, "Sony")
@@ -8,4 +10,10 @@ describe("Unit Tests for Spiderman class", () => {
         expect(andrewGarfield.studio) .toBe("Sony")
 
     })
+    
+})
+
+test('2) Using the getInfo() method', () => {
+    const tomHolland = new Spiderman("Marvel Spiderman ", 25, "Tom Holland", 5, "Marvel")
+    expect(tomHolland.getInfo()) .toBe("Hey I'm Tom Holland from Marvel studio")
 })
