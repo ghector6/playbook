a8e7e7397ac12961aa354b6cfb2df5687f0dbd5c
ghector6 <ghectormus@gmail.com>
2022-04-27 19:10:44 -0500

    Paso 9: Completando tercer metodo para ExplorerService

--- weekly_mission_4/FizzBuzz/lib/services/ExplorerServices.js
+++ weekly_mission_4/FizzBuzz/lib/services/ExplorerServices.js
@@ -1,18 +1,28 @@
 class ExplorerServices {
 
-    static filterByMission(explorers, mission){
+    static filterByMission(explorers, mission) {
 
         const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
         return explorersInNode
 
     }
 
-    static getAmountOfExplorersByMission(explorers, mission){
+    static getAmountOfExplorersByMission(explorers, mission) {
 
         const explorersInNode = explorers.filter((explorer) => explorer.mission === "node")
         return explorersInNode.length
 
     }
+
+    static getExplorersUserNameByMission(explorers, mission) {
+
+        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+        const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+        return usernamesInNode
+
+    }
+
+
 
 }
 
