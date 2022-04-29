3c85404f2804ca8c26f47b6c7e1edb76427e3d1e
ghector6 <ghectormus@gmail.com>
2022-04-28 22:05:01 -0500

    Paso 11: Agregando pruebas del ExplorerServices

--- weekly_mission_4/FizzBuzz/test/services/ExplorerServices.test.js
+++ weekly_mission_4/FizzBuzz/test/services/ExplorerServices.test.js
@@ -1,10 +1,23 @@
+const Reader = require("../../lib/utils/Reader");
 const ExplorerServices = require("./../../lib/services/ExplorerServices")
 
 describe("Test para ExplorerService", () => {
-    test("Requerimiento 1, Lista de Explorers que estan en mission de node", () => {
-        const explorers = [{ mission: "node"}]
+
+    test("Requerimiento 1 y 2, Prueba de que el static method filterByMission funcione y por ende el length del getAmountOfExplorersByMission", () => {
+        const explorers = Reader.readJsonFile("./explorers.json")
         const explorersInNode = ExplorerServices.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(1)
+        expect(explorersInNode.length).toBe(10)
 
     })
-})
+
+    test("Requerimiento 3, Prueba que imprima el nombre de ajolonauta en mission de node", () => {
+        const explorers = Reader.readJsonFile("./explorers.json")
+        const nameByMission = ExplorerServices.getExplorersUserNameByMission(explorers, "node");
+        expect(nameByMission[0]).toBe("ajolonauta1")
+    })
+    
+
+        
+        
+
+    })
