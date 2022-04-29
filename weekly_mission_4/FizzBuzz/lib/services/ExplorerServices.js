39945215b850951ae857844457d279712993c466
ghector6 <ghectormus@gmail.com>
2022-04-28 22:06:10 -0500

    Paso 12: Agregando ExplorerServices con sus static probados

--- weekly_mission_4/FizzBuzz/lib/services/ExplorerServices.js
+++ weekly_mission_4/FizzBuzz/lib/services/ExplorerServices.js
@@ -1,22 +1,23 @@
+
 class ExplorerServices {
 
     static filterByMission(explorers, mission) {
 
-        const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
+        const explorersInNode = explorers.filter((explorer) => explorer.mission == mission);
         return explorersInNode
 
     }
 
     static getAmountOfExplorersByMission(explorers, mission) {
 
-        const explorersInNode = explorers.filter((explorer) => explorer.mission === "node")
+        const explorersInNode = explorers.filter((explorer) => explorer.mission === mission)
         return explorersInNode.length
 
     }
 
     static getExplorersUserNameByMission(explorers, mission) {
 
-        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
+        const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == mission );
         const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
         return usernamesInNode
 
